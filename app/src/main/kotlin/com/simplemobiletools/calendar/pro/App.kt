e9b1d7ebd7dba98786d4324d4dbf937d200669ce
Yanni Chang <ccyanni@gmail.com>
2019-09-01 18:02:18 +0800

    Force the color config to fit EInk pannel

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/App.kt
@@ -1,11 +1,18 @@
 package com.simplemobiletools.calendar.pro
 
 import androidx.multidex.MultiDexApplication
+import com.simplemobiletools.commons.extensions.baseConfig
 import com.simplemobiletools.commons.extensions.checkUseEnglish
 
 class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         checkUseEnglish()
+        baseConfig.apply {
+            textColor = -16777216
+            backgroundColor = -1
+            primaryColor = -16777216
+            navigationBarColor = -16777216
+        }
     }
 }
