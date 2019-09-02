06f997040fbc99e476e7cca9d470a38ed0e9bb14
溫 琳傑 <s70487s@gmail.com>
2019-09-02 14:56:05 +0800

    Simple Calendar for EINK

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/App.kt
@@ -8,11 +8,11 @@ class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         checkUseEnglish()
-        baseConfig.apply {
-            textColor = -16777216
-            backgroundColor = -1
-            primaryColor = -16777216
-            navigationBarColor = -16777216
-        }
+//        baseConfig.apply {
+//            textColor = -16777216
+//            backgroundColor = -1
+//            primaryColor = -16777216
+//            navigationBarColor = -16777216
+//        }
     }
 }
